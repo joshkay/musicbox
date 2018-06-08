1c2fdb86f74550c20e53a3cfd375fdcb71841f0d
Josh Kay <josh.m.kay@gmail.com>
2018-06-07 23:32:51 -0400

    Removed unused import and alignment fixes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
--- src/components/Library.js
+++ src/components/Library.js
@@ -17,16 +17,16 @@ class Library extends Component
   {
     return (
       <section className="library">
-        {
-          this.state.albums.map((album, index) =>
-            <Link to={`/album/${album.slug}`} key={index}>
-              <img src={album.albumCover} alt={album.title} />
-              <div>{album.title}</div>
-              <div>{album.artist}</div>
-              <div>{album.songs.length} songs</div>
-            </Link>
-          )
-        }
+      {
+        this.state.albums.map((album, index) =>
+          <Link to={`/album/${album.slug}`} key={index}>
+            <img src={album.albumCover} alt={album.title} />
+            <div>{album.title}</div>
+            <div>{album.artist}</div>
+            <div>{album.songs.length} songs</div>
+          </Link>
+        )
+      }
       </section>
     );
   }  
